d2e44b943235f41b8e55e4b2f61a1388456cff92
d.ventsis <4aba3691@gmail.com>
2022-08-24 10:56:04 +0200

    Task2 commit Test Case 1 done Test Case 2 done Test Case 3 (Step 5 Need write asserts)...

--- Task1/src/test/java/UI_tests/BaseTest.java
+++ Task1/src/test/java/UI_tests/BaseTest.java
@@ -2,9 +2,7 @@ package UI_tests;
 
 import driver_manager.DriverManager;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 
